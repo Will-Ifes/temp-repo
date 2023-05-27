0c996cfee7cbed85dd8dc370fc120fb15fec37e5
Will-Ifes <wil77999@gmail.com>
2023-05-27 16:40:04 -0300

    first commit react bases

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+
+export const PostCard = ({title, cover, body, id}) =>  (
+    <div 
+    className='post'
+    >
+    <img src={cover} alt={title} />
+    <div  className='posts-content'>
+        <h2>{title} {id}</h2>
+        <p>{body}</p>
+    </div>
+    </div>
+);
